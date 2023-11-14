972f27dbaa81eb31598300684edd855f0297a8bb
eVTOL <87783102+quantumsynapse@users.noreply.github.com>
2023-11-13 22:26:23 -0300

    Update and rename Code_1 to Code_1.py

--- Code_1.py
+++ Code_1.py
@@ -1,4 +1,4 @@
-Configuração do Ambiente de Desenvolvimento:
+'''Configuração do Ambiente de Desenvolvimento:
 
     Instale Python, Pip e Git no Ubuntu usando comandos como sudo apt install python3 python3-pip git.
     Configure o Git (git config --global user.name "Seu Nome" e git config --global user.email "seuemail@example.com").
@@ -9,30 +9,28 @@ Implementação da Lógica de Coleta de Dados:
 
     Crie um arquivo data_collector.py.
     Escreva o código para simular a coleta de dados:
+'''
+import random
+from datetime import datetime, timedelta
 
-    python
-
-        import random
-        from datetime import datetime, timedelta
-
-        def simulate_daily_data(start_date, end_date):
-            current_date = start_date
-            while current_date <= end_date:
-                yield {
-                    'date': current_date.strftime('%d/%m/%Y'),
-                    'location': 'São Paulo',
-                    'total_aircrafts': random.randint(100, 200),
-                    'drones': random.randint(30, 60),
-                    'eVTOLs': random.randint(20, 40),
-                    'commercial_flights': random.randint(50, 100),
-                    'incidents': {
-                        'on_ground': random.randint(0, 5),
-                        'in_air': random.randint(0, 5)
-                    },
-                    'weather_conditions': {
-                        'visibility': random.choice(['Low', 'Medium', 'High']),
-                        'wind_speed': random.choice(['Low', 'Medium', 'High']),
-                        'precipitation': random.choice(['None', 'Light', 'Moderate', 'Heavy'])
+def simulate_daily_data(start_date, end_date):
+    current_date = start_date
+    while current_date <= end_date:
+        yield {
+                'date': current_date.strftime('%d/%m/%Y'),
+                'location': 'São Paulo',
+                'total_aircrafts': random.randint(100, 200),
+                'drones': random.randint(30, 60),
+                'eVTOLs': random.randint(20, 40),
+                'commercial_flights': random.randint(50, 100),
+                'incidents': {
+                'on_ground': random.randint(0, 5),
+                'in_air': random.randint(0, 5)
+        },
+        'weather_conditions': {
+        'visibility': random.choice(['Low', 'Medium', 'High']),
+                    'wind_speed': random.choice(['Low', 'Medium', 'High']),
+                    'precipitation': random.choice(['None', 'Light', 'Moderate', 'Heavy'])
                     },
                     'traffic_density': random.choice(['Low', 'Medium', 'High']),
                     'operational_issues': random.randint(0, 10)
